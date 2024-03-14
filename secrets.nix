5dfb34ac04338b06ca2816e7d0cd8887a9cb550d
uy_sun <hmy0119@gmail.com>
2024-03-14 08:27:19 +0800

    feat: add cloudflared key

---
+++ secrets.nix
@@ -0,0 +1,11 @@
+let
+  # Get system's ssh public key by command:
+  #    cat /etc/ssh/ssh_host_ed25519_key.pub
+  # If you do not have this file, you can generate all the host keys by command:
+  #    sudo ssh-keygen -A
+  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDUmKHI3210erAXq5h8oG7c3NOsXvxpnMNbElG3Hp/yx root@wsl";
+  systems = [ wsl ];
+in
+{
+  "./cloudflared.age".publicKeys = systems;
+}
