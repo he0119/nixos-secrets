a90766fb907f939935abef66457e16cd4fb68b86
uy_sun <hmy0119@gmail.com>
2024-03-17 10:05:59 +0800

    feat: add work-305

--- secrets.nix
+++ secrets.nix
@@ -6,12 +6,16 @@ let
   mini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDUmKHI3210erAXq5h8oG7c3NOsXvxpnMNbElG3Hp/yx root@mini";
   spin5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnksN9nCYFDVlvwfSW0U3yRSkwugxdlQSwyM9H1jV/+ root@spin5";
 
+  work-305 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBbVqEmQSBg+xt0bgsj+/mI7YzS26vJio8QVxfYodxQB root@work-305";
+
   miemie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzd9kDPZholuCKXVnOv8ouLryYL4P0L3ioClJTvTLtF root@miemie";
 
   recovery_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRrymJExf8Z+IJ8nf9qvGWAbIAdD/nsR8OoDQ9dH3F8 hmy01@HMY-SPIN5";
   systems = [
     mini
     spin5
+  
+    work-305
 
     miemie
 
